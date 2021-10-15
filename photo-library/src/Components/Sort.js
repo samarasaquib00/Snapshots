a37056d9fc15bdf9a177f462cd020542771d398f
samarasaquib00 <46725759+samarasaquib00@users.noreply.github.com>
2021-10-15 14:25:21 -0400

    updated sort

--- photo-library/src/Components/Sort.js
+++ photo-library/src/Components/Sort.js
@@ -1,12 +1,78 @@
-import React from 'react'
 import SortButton from './Sort.css'
+import Dropdown from './Dropdown.js';
+import React, { useState } from 'react';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function Sort() {
-    return (
-        <div className= 'sort button'>
-            <button class="SortButton">Sort</button>
+
+
+/*class Sort extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.ref = React.createRef();
+      }
+
+
+    myFunction() {
+        this.ref.current.classList.toggle("show");
+        //document.getElementById("myDropdown").classList.toggle("show");
+        console.log(this.ref.onclick)
+    }
+
+
+      render() {
+          return (
+        <div class="dropdown">
+        <button onclick={this.myFunction} ref={this.ref} class="SortButton">Sort</button>
+
+        <div id="myDropdown" class="drpdown-content">
+            <a href="#home">Sort by Date</a>
+            <a href="#about">Sort by Geolocation</a>
         </div>
-    )
+        </div>
+        
+          )
+      }
 }
 
-export default Sort
+
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.SortButton')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  */
+
+
+
+  const Sort = (props) => {
+    const [dropdownOpen, setOpen] = useState(false);
+  
+    const toggle = () => setOpen(!dropdownOpen);
+  
+    return (
+      <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle caret>
+          Sort
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem>Sort by Date</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Sort by Geolocation</DropdownItem>
+        </DropdownMenu>
+      </ButtonDropdown>
+    );
+  }
+  
+
+  export default Sort
