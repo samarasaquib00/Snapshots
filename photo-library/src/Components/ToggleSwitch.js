746dd4a3351302a1746d03c3834d1a432584561c
rachelhawley <hawley6@purdue.edu>
2021-10-31 21:25:38 -0400

    Settings Page

--- photo-library/src/Components/ToggleSwitch.js
+++ photo-library/src/Components/ToggleSwitch.js
@@ -5,8 +5,8 @@ const ToggleSwitch = ({ isOn, handleToggle }) => {
     return (
       <>
         <input checked={isOn} onChange={handleToggle} className="react_switch_checkbox" id={`react_switch_new`} type="checkbox"/>
-        <label className="react_switch_label" htmlFor={`react_switch_new`}>
-          <span className={`react_switch_button`} />
+        <label style={{ background: isOn && '#06D6A0' }} className="react_switch_label" htmlFor={`react_switch_new`}>
+          <div className={`react_switch_button`} />
         </label>
       </>
     );
