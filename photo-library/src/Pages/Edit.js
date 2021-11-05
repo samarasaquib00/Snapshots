b9a6aa2ecd121902ada50378fb0624b0db6fd803
Samara Saquib <46725759+samarasaquib00@users.noreply.github.com>
2021-11-05 13:35:52 -0400

    Edit window photo preview

--- photo-library/src/Pages/Edit.js
+++ photo-library/src/Pages/Edit.js
@@ -3,17 +3,26 @@ import './Edit.css'
 import Crop from '../Components/CropButton'
 import Exposure from '../Components/ExposureButton'
 import SaveExit from '../Components/SaveExitButton'
+import Filter from '../Components/FilterButton'
+import { useLocation } from 'react-router-dom'
 
 
 function Edit() {
+    const {location, query} = useLocation()
+    //const { target } = location.state
+    console.log(query)
     return (
         <div className='edit_page'>
-            <h1>Edit Photo Page</h1>
+            <div className='edit_page_header' >
+                <h1>Edit Photo Page</h1>
+                <SaveExit />
+            </div>
             <div className='edit'>
-                <h1>Click on edit options to edit photo</h1>
+                <p>Click on edit options to edit photo</p>
+                <img src={query.targetsrc} />
                 <Crop />
                 <Exposure />
-                <SaveExit />
+                <Filter />
 
             </div>
 
