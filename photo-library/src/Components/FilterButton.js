7eb1502942a9d23433672e809911c8c250867cdc
Samara Saquib <46725759+samarasaquib00@users.noreply.github.com>
2021-11-05 13:34:13 -0400

    filter button

---
+++ photo-library/src/Components/FilterButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FilterButton from './FilterButton.css'
+import { Link } from 'react-router-dom';
+
+
+function Filter() {
+    return (
+        <div className= 'crop button'>
+                <button class="FilterButton">Filter</button>
+        </div>
+    )
+}
+
+export default Filter
