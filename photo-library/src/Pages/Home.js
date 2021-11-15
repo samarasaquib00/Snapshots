1e1a54f80a54d46bdf9afc72fd8900310dd89f43
rachelhawley <hawley6@purdue.edu>
2021-11-14 21:02:05 -0500

    Added to the Home Page

--- photo-library/src/Pages/Home.js
+++ photo-library/src/Pages/Home.js
@@ -5,7 +5,10 @@ function Home() {
         <div className= 'home_page'>
             <h1>Home Page</h1>
             <h2>Welcome to Snapshots!</h2>
-            <p>To Sign Up or Login, click on the Profile icon on the top right.</p>
+            <p>To Sign Up or Login, click on the Profile icon on the top right and select Login or Sign Up.</p>
+            <p>After you login, you can then upload your photos to the program by clicking on the Upload tab in the sidebar.</p>
+            <p>Your uploaded photos can then be viewed by clicking on the Photo Library tab in the sidebar.</p>
+            <p>You can also create and view albums of your photos by clicking on the Albums tab in the sidebar.</p>
         </div>
     )
 }
